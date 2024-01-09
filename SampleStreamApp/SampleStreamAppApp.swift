545d864c17335d197c7f3eae88d40a6625836bf8
Russell Lowry <lowryrussell@gmail.com>
2024-01-09 05:59:47 -0600

    Initial Commit

---
+++ SampleStreamApp/SampleStreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleStreamAppApp.swift
+//  SampleStreamApp
+//
+//  Created by Russell Lowry on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleStreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
